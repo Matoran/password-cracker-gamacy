1af072fe18f66972f06b0552afcfbf3ff9c43614
Marco Lopes <marco.lopes05@gmail.com>
2016-10-24 22:19:30 +0200

    update last push

--- string.h
+++ string.h
@@ -1,8 +1,9 @@
 #ifndef STRING_H
 #define STRING_H
 #define ALPHABET "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!*"
+#define LENGTH_ALPHABET 65
 
-char* jumpToAlphabet(unsigned long long int nb, int nbAlphabet);
+char* jumpToAlphabet(unsigned long long int nb, char *result);
 char* inverseString(char* string);
-    
+
 #endif
