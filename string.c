1eb5ffe42fca078047baaf46251233d498f28401
Marco Lopes <marco.lopes05@gmail.com>
2016-10-24 22:13:38 +0200

    add end string character

--- string.c
+++ string.c
@@ -1,32 +1,30 @@
 #include "string.h"
 #include <string.h>
 #include <stdlib.h>
-    
+
 /*char[] nextString(char[] actualString){
-        
+
 }*/
 
-char* jumpToAlphabet(unsigned long long int nb, int nbAlphabet){
+char* jumpToAlphabet(unsigned long long int nb, char *result){
 	//char letters[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!*";
 	//char letters[] = "01";
-    char* result = malloc(sizeof(char)*20);
-	result[19] = '\0';
     unsigned long long int integerDivision = nb;
     unsigned long long int mod = 0;
 	unsigned long long int counter = 0;
     while(1){
-        mod = integerDivision % nbAlphabet;
-        integerDivision /= nbAlphabet;
+        mod = integerDivision % LENGTH_ALPHABET;
+        integerDivision /= LENGTH_ALPHABET;
 
         if(mod == 0){
-            mod = nbAlphabet;
+            mod = LENGTH_ALPHABET;
             integerDivision--;
         }
 
         if(integerDivision  == 0){
 			//printf("%d\n",mod);
             result[counter] = ALPHABET[mod-1];
-
+            result[counter+1] = '\0';
 			result = inverseString(result);
 
             return result;
@@ -39,12 +37,12 @@ char* jumpToAlphabet(unsigned long long int nb, int nbAlphabet){
 
 char* inverseString(char* string){
 	int length = strlen(string);
-	char tmpChar;	
+	char tmpChar;
 
 	for(int i = 0; i < (length/2); i++){
 		tmpChar = string[i];
 		string[i] = string[length-1-i];
-		string[length-1-i] = tmpChar;	
+		string[length-1-i] = tmpChar;
 	}
 	return string;
 }
