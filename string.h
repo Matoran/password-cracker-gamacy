93083c6874047ff896b0dfad15a285447801d999
matoran <marco.rodrigues-lopes@etu.hesge.ch>
2016-11-02 13:54:46 +0100

    add alphabet

--- string.h
+++ string.h
@@ -1,7 +1,7 @@
 #ifndef STRING_H
 #define STRING_H
-#define ALPHABET "ABC"
-#define LENGTH_ALPHABET 3
+#define ALPHABET "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!*"
+#define LENGTH_ALPHABET 65
 
 char *jumpToAlphabet(unsigned long long int nb, char *result);
 
